435140edd5493bc85272fdf8c107aaa12b863c9f
Lirik-Keltran <keltran.987@gmail.com>
2024-01-30 17:13:41 +1100

    сохранение данных

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { parseArgs } from "util";
 import decode from "base32-decode"
 import jsSHA from "jssha";
+import { pipe } from "fp-ts/lib/function";
+import * as O from "fp-ts/Option";
+import * as E from "fp-ts/Either";
+import * as TE from "fp-ts/TaskEither";
+import * as T from "fp-ts/Task";
+
+type Config = {
+  codes: Array<string>,
+};
 
 const { values  } = parseArgs({
   args: Bun.argv,
@@ -14,9 +23,65 @@ const { values  } = parseArgs({
   allowPositionals: true,
 });
 
-if(values.code === undefined) {
-  process.exit(1);
+const value = values.code as string;
+
+
+const saveAndGetConfig = (code: string | undefined): TE.TaskEither<Error, Config> => {
+
+  const read = (): TE.TaskEither<Error, Config> => {
+    return TE.tryCatch(
+      () => pipe(
+        Bun.file("config/code.json"),
+        file => file.json<Config>()
+      ),
+      E.toError
+    );
+  };
+
+  const save = (code: string): TE.TaskEither<Error, Config> => {
+    const deafultConfig = {
+      codes: [code]
+    }
+
+    return pipe(
+      read(),
+      TE.map(config => config.codes.at(-1) === code ? config : ({ codes: [...config.codes, code] })),
+      TE.flatMap(config =>
+        TE.fromTask(
+          () => Bun.write("config/code.json", JSON.stringify(config)).then(() => config),
+        )
+      ),
+      TE.orElse(() =>
+        TE.fromTask(
+          () => Bun.write("config/code.json", JSON.stringify(deafultConfig)).then(() => deafultConfig),
+        )
+      )
+    )
+  }
+
+
+  return pipe(
+    code,
+    O.fromNullable,
+    O.fold(
+      read,
+      save
+    )
+  );
 }
+
+const config = await pipe(
+  saveAndGetConfig(value),
+  TE.fold(
+    (e) => {
+      console.error(e);
+      process.exit(1);
+    },
+    config => T.of(config),
+  )
+)();
+
+const code = config.codes[config.codes.length-1];
 
 const uintToBuf = (num: number) => {
   const buf = new ArrayBuffer(8);
@@ -35,9 +100,10 @@ const uintToBuf = (num: number) => {
 
 const getTOTP = (code: string) => {
   const time = Math.floor(Date.now() / 1000 / 30);
+  const timeToUpdate = Math.floor((Date.now() / 1000 % 30))
+
   const count = uintToBuf(time)
   const key = decode(code, "RFC3548");
-
   const hmac = new jsSHA("SHA-1", "ARRAYBUFFER");
   hmac.setHMACKey(key, "ARRAYBUFFER")
   hmac.update(count);
@@ -51,7 +117,10 @@ const getTOTP = (code: string) => {
       | ((hash[offset + 2] & 0xFF) << 8)
       | (hash[offset + 3] & 0xFF)) % 10 ** 6;
 
-  return otp.toString().padStart(6, "0");
-}
+  return {
+    TOTP: otp.toString().padStart(6, "0"),
+    timeToUpdate
+  };
+};
 
-console.log(getTOTP(values.code as string))
+console.log(JSON.stringify(getTOTP(code)))
